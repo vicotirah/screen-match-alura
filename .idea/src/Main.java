3a09efb369992ace66af5c7150d26954e9e03188
Vicotirah <victoria_rocha_26@hotmail.com>
2026-06-19 23:15:19 -0300

    [feat] Estrutura inicial do projeto

---
+++ .idea/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println(anoDeLancamento);
+        String sinopse = "Sinopse";
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        double media = (9.8 + 6.3 + 8) / 3;
+        int classificacao = (int) (media/ 2);
+        System.out.println(classificacao);
+
+    }
+}
